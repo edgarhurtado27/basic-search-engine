e31d26ad11f8adb48fb5676a6b77ceea30a0e382
Edgar <edgarivancs@gmail.com>
2021-07-22 12:09:50 -0500

    feat: SearchResult code refactor

--- src/components/SearchResult/SearchResult.js
+++ src/components/SearchResult/SearchResult.js
@@ -1,38 +1,44 @@
 import React from 'react'
-import {Grid, Typography, ListItem} from '@material-ui/core';
+import {Grid, Typography, List, ListItem} from '@material-ui/core';
 
-const renderResult = (result) => {
+const renderResult = ({text, title, id}) => {
   return (
-    result.map(({text, title, id}) => (
-      <ListItem key={id} button>
-        <Grid
+    <ListItem key={id} button>
+      <Grid
+        container
+        justifyContent="space-between"
+        direction="row">
+
+        <Grid item xs={10}
           container
-          justifyContent="space-between"
-          direction="row">
-
-          <Grid item xs={10}
-            container
-            justifyContent="center"
-            direction="column"
-            alignItems="flex-start">
-            <Typography display="inline" variant="h4">{title}</Typography>
-            <Typography display="inline" variant="subtitle1">{text}</Typography>
-          </Grid>
-          <Grid item xs={2}
-            container
-            justifyContent="center"
-            direction="column"
-            alignItems="center">
-            <Typography display="inline" variant="h3">{id}</Typography>
-          </Grid>
-
+          justifyContent="center"
+          direction="column"
+          alignItems="flex-start">
+          <Typography display="inline" variant="h4">{title}</Typography>
+          <Typography display="inline" variant="subtitle1">{text}</Typography>
         </Grid>
-      </ListItem>
-    ))
+        <Grid item xs={2}
+          container
+          justifyContent="center"
+          direction="column"
+          alignItems="center">
+          <Typography display="inline" variant="h3">{id}</Typography>
+        </Grid>
+
+      </Grid>
+    </ListItem>
   )
 }
 
-const SearchResult = ({result}) => result && result.length > 0 && renderResult(result)
+const SearchResult = ({result}) => {
+  return (
+    <List>
+      {
+        result && result.length > 0 && result.map(item => renderResult(item))
+      }
+    </List>
+  )
+}
 
 export default SearchResult
 
