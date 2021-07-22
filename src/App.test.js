8e6068ab6be97ea1bda43577f732af852ff8cee3
Edgar <edgarivancs@gmail.com>
2021-07-22 17:57:24 -0500

    fix: Unit test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Buscador basico/i);
+  const linkElement = screen.getByText(/Buscar bibliografia/i);
   expect(linkElement).toBeInTheDocument();
 });
